6cece02ff313b47669584ce5627e18161b316945
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-08-11 23:20:07 +0100

    Add fileName function to index.js

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ const fs = require('fs');
 
 const PORT = 8080;
 
+function fileName(url) {
+    //We remove the first character ('/') 
+    let fileName = url.slice(1);
+    switch (url) {
+        case '/':
+            fileName = 'index.html';
+            break;
+        case '/about':
+            fileName = 'about.html';
+            break;
+        case '/contact-me':
+            fileName = 'contact-me.html';
+            break;
+    } 
+    return fileName;
+}
+
 const server = http.createServer((req, res) => {
     //Handle request here ...
+    const reqUrl = req.url;
+    console.log(fileName(reqUrl));
     res.end('Hello World!');
 })
 
